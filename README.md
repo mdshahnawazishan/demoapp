cde208e8cd4c4c0508864aa7f83af0bd3c4b06b8
Md Shahnawaz <ishan.shahnawaz@gmail.com>
2018-06-03 00:55:24 +0530

    Second commit

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Notes
+
+ng build --prod --output-path docs --base-href PROJECT_NAME
+
+copy index.html to 404.html
+
+ngh
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  dummyJson: string;
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  fetchJSON() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe(data => {
+      this.dummyJson = JSON.stringify(data)
+    })
+    
+  }
 }
